141ecaa80e0e01bed01fd7ce22df58eb31cec509
greenyeast <djaks65@gmail.com>
2023-05-15 00:13:31 +0900

    Test the code with Junit, error issue

---
+++ src/main/java/org/exmaple/springboot/web/HelloController.java
@@ -0,0 +1,18 @@
+package org.exmaple.springboot.web;
+import org.exmaple.springboot.web.dto.HelloResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDTO helloDTO(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDTO(name, amount);
+    }
+}
---
+++ src/main/java/org/exmaple/springboot/web/dto/HelloResponseDTO.java
@@ -0,0 +1,10 @@
+package org.exmaple.springboot.web.dto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDTO {
+    private final String name;
+    private final int amount;
+}
